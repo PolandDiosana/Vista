7e24cc71fc01073d20b9be3ea7b6b6e7d4d78804
Deon <Deon@DESKTOP-J3B3NMF>
2025-03-16 10:38:07 +0800

    ga puno ug  dashboard model

---
+++ Pages/homeOwner/Dashboard.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vista_Subdivision.Pages.homeOwner
+{
+    public class DashboardModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
