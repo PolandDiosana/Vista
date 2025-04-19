7bf8c7c93840453c6dba1c4dd0641abecdce81a1
JersonMraz <jersonsullano201@gmail.com>
2025-04-19 23:17:01 +0800

    Save changes locally before pulling

---
+++ Pages/Officer/Complaints.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Vista_Subdivision.Pages.Officer
+{
+    public class ComplaintsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
